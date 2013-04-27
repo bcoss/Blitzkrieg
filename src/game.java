3c87a21cad569943ed98acc9cac3249493367696
Jonathan Gautney <Darknesstube@gmail.com>
2013-04-27 13:42:15 -0400

    test map

--- src/game.java
+++ src/game.java
@@ -1,34 +1,38 @@
 import org.newdawn.slick.*;
+import org.newdawn.slick.tiled.TiledMap;
 public class game extends BasicGame
 {
- 
-  public game()
-  {
-     super("Goodbye");
-  }
- 
-  @Override
-  public void init(GameContainer gc) throws SlickException
-  {
-	  gc.setShowFPS(false); 
-  }
- 
-  @Override
-  public void update(GameContainer gc, int delta) throws SlickException
-  {
- 
-  }
- 
-  @Override
-  public void render(GameContainer gc, Graphics g) throws SlickException
-  {
-  }
- 
-  public static void main(String[] args) throws SlickException
-  {
-     AppGameContainer app = new AppGameContainer(new game());
- 
-     app.setDisplayMode(800, 600, false);
-     app.start();
-  }
+
+	private TiledMap map;
+	public game()
+	{
+		super("Goodbye");
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException
+	{
+		gc.setShowFPS(false); 
+		map = new TiledMap("res/maps/test.tmx");
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException
+	{
+
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		map.render(0, 0);
+	}
+
+	public static void main(String[] args) throws SlickException
+	{
+		AppGameContainer app = new AppGameContainer(new game());
+
+		app.setDisplayMode(800, 600, false);
+		app.start();
+	}
 }
